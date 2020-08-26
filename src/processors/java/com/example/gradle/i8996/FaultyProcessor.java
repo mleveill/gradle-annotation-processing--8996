d3f7e7db6e1333f89691a27d204e2d0e097f4710
Brick Tamland <mleveill@users.noreply.github.com>
2020-08-25 22:31:17 -0400

    Cause error during processor class loading.

--- src/processors/java/com/example/gradle/i8996/FaultyProcessor.java
+++ src/processors/java/com/example/gradle/i8996/FaultyProcessor.java
@@ -1,5 +1,6 @@
 package com.example.gradle.i8996;
 
+import com.google.common.collect.ImmutableSet;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -11,6 +12,8 @@ import javax.lang.model.element.TypeElement;
 @SupportedAnnotationTypes(RunsFaultyProcessor.QUALIFIED_NAME)
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class FaultyProcessor extends AbstractProcessor {
+
+	private static final ImmutableSet<String> THIS_NEVER_RUNS = ImmutableSet.of();
 
 	public FaultyProcessor() {
 	}
