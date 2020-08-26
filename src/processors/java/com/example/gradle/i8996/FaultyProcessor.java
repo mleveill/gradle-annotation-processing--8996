885ab95535456d4007d866b03e05e86f3352365b
Brick Tamland <mleveill@users.noreply.github.com>
2020-08-25 21:21:01 -0400

    Try to reproduce partial NoClassDefFound error message from another user.

--- src/processors/java/com/example/gradle/i8996/FaultyProcessor.java
+++ src/processors/java/com/example/gradle/i8996/FaultyProcessor.java
@@ -9,15 +9,15 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 @SupportedAnnotationTypes(RunsFaultyProcessor.QUALIFIED_NAME)
-@SupportedSourceVersion(SourceVersion.RELEASE_8) // Could probably be as low as RELEASE_5
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class FaultyProcessor extends AbstractProcessor {
 
 	public FaultyProcessor() {
-		throw new RuntimeException("Whoops!");
 	}
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+		ProcessorHelper thisNeverRuns = new ProcessorHelper();
 		return false;
 	}
 }
---
+++ src/processors/java/com/example/gradle/i8996/ProcessorHelper.java
@@ -0,0 +1,8 @@
+package com.example.gradle.i8996;
+
+import com.google.common.collect.ImmutableSet;
+
+public class ProcessorHelper {
+
+	private static final ImmutableSet<String> THIS_NEVER_RUNS = ImmutableSet.of();
+}
